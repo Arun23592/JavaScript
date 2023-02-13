d56ff9f3cf4ae2a03a19bd72501ef707c6958a3c
Arun23592 <arunsubramani1992@gmail.com>
2023-02-13 22:44:46 +0530

    reversenumber

---
+++ ReverseNumber.js
@@ -0,0 +1,18 @@
+function reverseNum(n) {
+  let reversedNumber = 0; //reversedNumber is used to store the reverse value. It is  initialized as 0
+  let digit;
+  while (n != 0) {
+    //The while loop runs till the value of num is more than 0
+    let digit = n % 10;
+    reversedNumber = reversedNumber * 10 + digit;
+    n = Math.floor(n / 10);
+  }
+  return reversedNumber;
+  console.log("Reverse number : " + reversedNumber);
+}
+// NOTE: Please do not modify this function
+function main() {
+  let n = parseInt(readLine(), 10);
+  let result = reverseNum(n);
+  console.log(result);
+}
